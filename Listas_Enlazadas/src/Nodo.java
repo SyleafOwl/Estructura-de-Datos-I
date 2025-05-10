eedcb5e5227ce8e26265886460fa9529bd7973ba
David <syleafowl@gmail.com>
2025-05-10 17:05:04 -0500

    1.0

---
+++ Listas_Enlazadas/src/Nodo.java
@@ -0,0 +1,10 @@
+class Nodo {
+    int dato;       // Valor almacenado en el nodo
+    Nodo siguiente; // Referencia al siguiente nodo
+
+    // Constructor
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+}
