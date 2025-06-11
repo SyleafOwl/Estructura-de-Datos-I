ebc5fd62708c5bd11b2f75bb66fbcaf11a7ee4bc
David <syleafowl@gmail.com>
2025-06-11 17:05:53 -0500

    6.1

--- CircularPlaylist Double/NodoSongDouble.java
+++ CircularPlaylist Double/NodoSongDouble.java
@@ -1,12 +1,10 @@
 public class NodoSongDouble {
     String name;
-    String filePath;
     NodoSongDouble next;
     NodoSongDouble previous; 
 
     public NodoSongDouble(String name) {
         this.name = name;
-        this.filePath = null; 
         this.next = null;
         this.previous = null; 
     }
